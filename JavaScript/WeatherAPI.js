44622a58cf7d3e44bb7de9f78dae0d8508e38a6d
ShadowHawk2 <ajohnsen1234@gmail.com>
2022-03-14 12:27:37 +0100

    OG API

--- JavaScript/WeatherAPI.js
+++ JavaScript/WeatherAPI.js
@@ -1,37 +1,37 @@
 async function getWeather() {
-    let weather;
-    let hour;
     let temp;
+    let far;
     const api = await fetch("https://api.open-meteo.com/v1/forecast?latitude=59.78&longitude=5.49&hourly=temperature_2m&current_weather=true");
     const json = await api.json();
-    weather = json.hourly.temperature_2m;
-    hour = json.hourly.time;
     temp = json.current_weather.temperature;
+    far = (temp*1.8)+32;
 
-    //Chart
-    if (weather) {
-        document.getElementById("vær").innerHTML = weather;
+// Skjekker om temp er noe.
+    if (temp > -10000000000) {
+        document.getElementById("temp").innerHTML = "Det er " +temp + "°C no. " + "<br>" + "I Fahrenheit er det " + far + "F";
     }
-    if (hour) {
-        document.getElementById("time").innerHTML = hour;
+    
+// Får iformasjon om kva bilde som skal brukes.
+    if (temp >= 20 ) {
+        document.getElementById("vær").innerHTML = '<h2>20°C - Grader</h2><img class="størelse" id="shadow" src="Images/20-grader.jpg" alt="20-G">';
     }
-    if (temp) {
-        document.getElementById("temp").innerHTML = temp;
+
+    else if (temp < 20 && temp >= 15) {
+        document.getElementById("vær").innerHTML = '<h2>15°C - Grader</h2><img class="størelse" id="shadow" src="Images/15-grader.jpg" alt="15-G">';
     }
+
+    else if (temp < 15 && temp > 1) {
+        document.getElementById("vær").innerHTML = '<h2>10°C - Grader</h2><img class="størelse" id="shadow" src="Images/10-grader.jpg" alt="10-G">';
+    }
+
+    else {
+        document.getElementById("vær").innerHTML = '<h2>Minus°C - Grader</h2><img class="størelse" id="shadow" src="Images/minus - grader.jpg" alt="Minus-G">'
+    }
+
 }
 getWeather();
 
-
-function badevær() {
-    if (temp < 20 ) {
-        document.getElementById("").innerHTML = '<h2>20 - Grader</h2><img class="størelse" src="Images/20-grader.jpg" alt="20-G">';
-    }
-
-    if (temp < 15 || temp < 10) {
-        document.getElementById("").innerHTML = '<h2>15 - Grader</h2><img class="størelse" src="Images/15-grader.jpg" alt="15-G">';
-    }
-
-    if (temp > 10) {
-        document.getElementById("").innerHTML = '<h2>20 - Grader</h2><img class="størelse" src="Images/20-grader.jpg" alt="20-G">';
-    }
-}
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML = (valNum*1.8)+32 + "F";
+  }
